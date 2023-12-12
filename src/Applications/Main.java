83a00f709cd9a16892f0a2eeefbf5430a2cd7b5b
sachinsingh <sachinseengh@gmail.com>
2023-12-12 09:28:27 +0545

    Scene Switcher in javafx

---
+++ src/Applications/Main.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package Applications;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sachi
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+     try{
+        Parent root;
+         root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+        Scene scene = new Scene(root);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }catch(Exception e){
+        e.printStackTrace();
+    }
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/Applications/SceneController.java
@@ -0,0 +1,41 @@
+
+package Applications;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class SceneController {
+
+   
+    private Stage stage;
+    private Scene scene;
+    private Parent roots;
+    
+    
+    
+    public void switchToScene1(ActionEvent event) throws IOException{
+          
+         Parent roots = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+         stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+         scene= new Scene(roots);
+         stage.setScene(scene);
+         stage.show();
+    }
+    
+    public void switchToScene2(ActionEvent event) throws IOException{
+        Parent roots = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
+         stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+         scene= new Scene(roots);
+         stage.setScene(scene);
+         stage.show();
+    }
+    public static void main(String args[]) {
+        
+    }
+}
