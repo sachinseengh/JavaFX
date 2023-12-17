ceefb73b74923abe05b94eee8e45476489728b46
sachinsingh <sachinseengh@gmail.com>
2023-12-17 19:25:42 +0545

    Temperature slider in javafx

---
+++ src/Slider/Main.java
@@ -0,0 +1,32 @@
+
+package Slider;
+
+import java.io.IOException;
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+       Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+  
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
