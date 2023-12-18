59cdbd6fffd139753d2b2d45daeeb2672db421ba
sachinsingh <sachinseengh@gmail.com>
2023-12-18 19:25:17 +0545

    ListView in javafx

---
+++ src/ListView/FXMLController.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+/**
+ * FXML Controller class
+ *
+ * @author sachi
+ */
+public class FXMLController implements Initializable {
+
+    @FXML
+    private ListView myListView;
+       
+    @FXML
+    private Label myLabel;
+    
+    
+    String food[]={"Dhal","Bhat","Tarkari"};
+    String currentValue;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       myListView.getItems().addAll(food);
+       
+       
+       myListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>(){
+           @Override
+           public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+             currentValue= (String) myListView.getSelectionModel().getSelectedItem();
+             
+             myLabel.setText(currentValue);
+           }
+           
+       
+       
+       
+       
+       });
+    }    
+    
+}
