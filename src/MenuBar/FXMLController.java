acee441b5b02187176403aca098fd5e7a5b1884f
sachinsingh <sachinseengh@gmail.com>
2023-12-18 21:09:05 +0545

    MenuBar in javafx

---
+++ src/MenuBar/FXMLController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package MenuBar;
+
+
+
+/**
+ * FXML Controller class
+ *
+ * @author sachi
+ */
+public class FXMLController {
+
+    
+    
+    public void newClicked(){
+        System.out.println("You clicked New ");
+    }
+   
+}
