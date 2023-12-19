8f91088e4a312720c6c7872cbd57459e6af8a654
sachinsingh <sachinseengh@gmail.com>
2023-12-19 08:34:19 +0545

    Flowpane in javafx

---
+++ src/FlowPane/Main.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package FlowPane;
+
+import java.io.IOException;
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sachi
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+      
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        primaryStage.setTitle("Flow Pane!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
