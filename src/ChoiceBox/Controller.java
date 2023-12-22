9b6eaf7d16fde5cf05852c1d9b3e4bc6871a7b64
sachinsingh <sachinseengh@gmail.com>
2023-12-22 19:18:34 +0545

    modified method used

--- src/ChoiceBox/Controller.java
+++ src/ChoiceBox/Controller.java
@@ -1,43 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package ChoiceBox;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    
     @FXML
-    private ChoiceBox myChoiceBox;
-    
-    @FXML
-    
-    private Label myLabel;
-    
-    
-    String []arr={"Pizza","Burger","MoMo"};
+    private ChoiceBox<String> myChoiceBox;  // Specify the type of ChoiceBox
 
-    
+    @FXML
+    private Label myLabel;
+
+    String[] arr = {"Pizza", "Burger", "MoMo"};
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
         myChoiceBox.getItems().addAll(arr);
 
-//        myChoiceBox.setOnAction(this::getFood);
-    }    
-    
-    public void getFood(ActionEvent e){
-        String myFood =(String) myChoiceBox.getValue();
-        myLabel.setText(myFood);
+        // Add a listener to the ChoiceBox to handle changes
+       myChoiceBox.getSelectionModel().selectedItemProperty().addListener((observerable,oldValue,newValue)->{
+       
+       
+        if(newValue != null){
+           updateLabel(newValue);}
+       
+       });
+      
+       
+    }
+
+  
+
+    private void updateLabel(String newValue) {
+        myLabel.setText(newValue);
     }
 }
